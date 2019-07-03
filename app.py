6cd4d7b1e065610c44e7aed7f35ed64a08a81bbc
matthewmuirhead <matthew.muirhead@live.co.uk>
2019-07-03 15:21:22 +0100

    added comments

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+#import necessary libraries
 from flask import Flask, request
 from flask_restful import Resource, Api
 import json
 
+#create instance of flask framework
 app = Flask(__name__)
 api = Api(app)
 
+#create function to process data
 class Get_Json(Resource):
     def get(self, value):
-        
+
+        #check the value to return corresponding answer
         if (value % 7 == 0) and (value % 9 == 0):
             result = {"result":"CN"}			
         elif (value % 7 == 0):
@@ -16,11 +20,14 @@ class Get_Json(Resource):
             result = {"result":"N"}
         else:
             result = {"result":value}
+
+        #return answer in json format
         return json.dumps(result)
 
+#add path to call function
 api.add_resource(Get_Json, '/GET/<value>')
 
-
+#run api if it is main
 if __name__ == '__main__':
-     app.run(port='80')
+    app.run(port='80')
      
